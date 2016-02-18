17bfe6824721ce947c2d8effa93665c3469e58a5
Vincent Flesouras <x.algorithm@gmail.com>
2016-02-18 19:23:14 +0000

    changing the way boostrap works

--- bootstrap-test.sh
+++ bootstrap-test.sh
@@ -110,7 +110,12 @@ sudo /usr/local/bin/pip install pygithub
 
 setStatusMessage "Get SLAC configs"
 #Get the required configs
-	echo "Getting the correct configs/n"
+	if [ -d "~/.slac-mac" ]; then
+	    setStatusMessage " I see you've done this before, Refreshing the SLAC configs/n"
+	    rm -Rf ~/.slac-mac
+	    git clone -q https://github.com/SLAC-ocio/mac-dev-deployment ~/.slac-mac/
+	else
+
 	mkdir -p ~/.slac-mac
 	git clone -q https://github.com/SLAC-ocio/mac-dev-deployment ~/.slac-mac/
 
@@ -124,8 +129,9 @@ sudo chgrp -R admin /usr/local
 
 if [ -d "/usr/local/grail/config" ]; then
     setStatusMessage "Update your config from git"
-    cd /usr/local/grail/config
-    git pull -q
+    rm -Rf /usr/local/grail/config
+    git clone -q https://github.com/slac-ocio/grail-config.git /usr/local/grail/config
+
 else
     setStatusMessage "Getting your config from your fork"
     git clone -q https://github.com/slac-ocio/grail-config.git /usr/local/grail/config
